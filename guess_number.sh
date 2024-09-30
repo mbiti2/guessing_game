29a5424f4ca6c75408ecaa30e400a4c3be1d2c17
mbiti2 <louisembiti@gmail.com>
2024-09-30 15:48:13 +0100

    added a space

--- guess_number.sh
+++ guess_number.sh
@@ -8,7 +8,7 @@ guessing_number() {
         if [ $rand_number -eq $guest_number ]; then
             echo "You won!"
             break
-        elif [ $rand_number -gt $guest_number ]; then
+         elif [ $rand_number -gt $guest_number ]; then
             echo "You missed! The random number is greater than $guest_number."
             read -p "Please try again: " guest_number
         else
