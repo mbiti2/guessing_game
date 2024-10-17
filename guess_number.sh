1b6230c5c8858904cdd1cf956ee5848e125e7ade
mbiti2 <louisembiti@gmail.com>
2024-10-17 14:16:09 +0100

    modified version

--- guess_number.sh
+++ guess_number.sh
@@ -1,13 +1,15 @@
 guessing_game() {
     rand_number=$((RANDOM % $1))
+    guess_numbers=($2 $3 $4) #read guess numbers
     for i in {1..3}; do
-        echo "Guess the number:" #prompt the user to enter a value
-        read guess_number #read input from the user
+    guess_number=${guess_numbers[ $i ]}
+        #echo "Guess the number:" #prompt the user to enter a value
+        #read guess_number #read input from the user
 
-        if [ $rand_number -eq $guess_number ]; then
+        if [ "$rand_number" -eq "$guess_number" ]; then
             echo "Congratulations, You Won!"
             return
-        elif [ $rand_number -gt $guess_number ]; then
+        elif [ "$rand_number" -gt "$guess_number" ]; then
              echo "You missed! The random number is greater than $guess_number"
         else
             echo "You missed! The random number is lesser than $guess_number"
